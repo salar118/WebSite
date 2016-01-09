e705f1ae6f569ce3e9cdb74e759860bb8855c7a6
Salar Abdefaily <salar118@yahoo.com>
2016-01-09 18:23:51 +0100

    Committing all the files!

---
+++ client/app/app.constant.js
@@ -0,0 +1,9 @@
+(function(angular, undefined) {
+'use strict';
+
+angular.module('webSiteApp.constants', [])
+
+.constant('appConfig', {userRoles:['guest','user','admin']})
+
+;
+})(angular);
---
+++ client/app/app.js
@@ -0,0 +1,16 @@
+'use strict';
+
+angular.module('webSiteApp', [
+  'webSiteApp.constants',
+  'ngCookies',
+  'ngResource',
+  'ngSanitize',
+  'ui.router',
+  'ui.bootstrap'
+])
+  .config(function($urlRouterProvider, $locationProvider) {
+    $urlRouterProvider
+      .otherwise('/');
+
+    $locationProvider.html5Mode(true);
+  });
